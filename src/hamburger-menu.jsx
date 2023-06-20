aab1a757e0a9279d7515cd00dcb0ce4dd7a0e070
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2023-06-19 21:00:23 -0600

    correciones en el menu hamburguesa again

--- src/hamburger-menu.jsx
+++ src/hamburger-menu.jsx
@@ -2,42 +2,56 @@ import { useState } from 'react';
 import React from 'react';
 import './styles/hamburger.css';
 
-const hamburger = () => {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const toggleMenu = () => {
-      setIsOpen(!isOpen);
-    };
-  
-    return (
-      <div className='container-hamburger'>
-        <div className="hamburger-menu">
-          <div className={`menu-container ${isOpen ? 'open' : ''}`}>
-            <button className="menu-toggle" onClick={toggleMenu}>
-              <span className={`bar ${isOpen ? 'open' : ''}`}></span>
-              <span className={`bar ${isOpen ? 'open' : ''}`}></span>
-              <span className={`bar ${isOpen ? 'open' : ''}`}></span>
-            </button>
-          </div>
-          {isOpen && (
-            <div className="menu">
-              <div className="contamination-icon">
-                <img src="src\assets\contamination-icon.svg" alt="" className='contamination-image'/>
-                <a href="/contamination" className='contamination-txt'>Contaminación</a>
-              </div>
-              <div className="register-icon">
-                <img src="src\assets\register-icon.svg" alt="" className="register-image" />
-                <a href="/acerca" className='register-txt'>Registro</a>
-              </div>
-              <div className="pronostico-icon">
-                <img src="src\assets\pronostico-icon.svg" alt="" className="pronostico-image"/>
-                <a href="/servicios" className='pronostico.txt'>Pronostico</a>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-    );
+const Hamburger = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
-  
-  export default hamburger;
+
+  return (
+    <div className="container-hamburger">
+      <div className={`hamburger-menu ${isOpen ? 'open' : ''}`}>
+        <div className="menu-container">
+          <button className={`menu-toggle ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+            {isOpen ? (
+              <>
+                <img src="src\assets\mac-login-white.png" alt="Menu Open" className="mac-icon-home" />
+              </>
+            ) : (
+              <>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+              </>
+            )}
+          </button>
+        </div>
+        {isOpen && (
+          <div className="menu">
+            <div className="contamination-icon">
+              <img src="src/assets/contamination-icon.svg" alt="Contamination Icon" className="contamination-image" />
+              <a href="/" className="contamination-txt">
+                Contaminación
+              </a>
+            </div>
+            <div className="register-icon">
+              <img src="src/assets/register-icon.svg" alt="Register Icon" className="register-image" />
+              <a href="/" className="register-txt">
+                Registro
+              </a>
+            </div>
+            <div className="pronostico-icon">
+              <img src="src/assets/pronostico-icon.svg" alt="Pronostico Icon" className="pronostico-image" />
+              <a href="/" className="pronostico-txt">
+                Pronostico
+              </a>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Hamburger;

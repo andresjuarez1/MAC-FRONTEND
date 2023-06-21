92ee04918ddfea1f9b6b2f52bf275ae6337635a1
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2023-06-21 08:16:01 -0600

    menu hamburguesa cambios

--- src/home.jsx
+++ src/home.jsx
@@ -18,21 +18,23 @@ const Home = () => {
         <img src="src\assets\cloud-home.png" alt="" className='cloud-home' />
         <h1 className='temperature-data-home'>50°</h1>
       </div>
-
-      <div className="cards">
-        <div className="card1">
-          <img src="src\assets\cloud-home.png" alt="" className='card1-img'/>
-          <h1 className="card1-txt">17:00 hrs</h1>
-        </div>
-        <div className="card2">
-          <img src="src\assets\cloud-home.png" alt="" className='card2-img'/>
-          <h1 className="card2-txt">16:00 hrs</h1>
-        </div>
-        <div className="card3">
-          <img src="src\assets\cloud-home.png" alt="" className='card3-img'/>
-          <h1 className="card3-txt">15:00 hrs</h1>
+      <div className="container-cards">
+        <div className="cards">
+          <div className="card1">
+            <img src="src\assets\cloud-home.png" alt="" className='card1-img' />
+            <h1 className="card1-txt">17:00 hrs</h1>
+          </div>
+          <div className="card2">
+            <img src="src\assets\cloud-home.png" alt="" className='card2-img' />
+            <h1 className="card2-txt">16:00 hrs</h1>
+          </div>
+          <div className="card3">
+            <img src="src\assets\cloud-home.png" alt="" className='card3-img' />
+            <h1 className="card3-txt">15:00 hrs</h1>
+          </div>
         </div>
       </div>
+
       <div className="login-home-icon">
         <a href="/" className="login-icon"></a>
         {/* <img src="src\assets\login.png" alt="" className='login-icon'/> */}

ccb7b18e4053b566d923aa95751385fb2243f8a8
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2023-06-22 11:08:19 -0600

    pagina de contaminacion

--- src/history.jsx
+++ src/history.jsx
@@ -4,11 +4,23 @@ import './styles/history.css';
 
 const History = () => {
   return(
-    <div className='historyy'>
+    <div className='history'>
       <div className="container-history">
-      <Hamburger3 />
+        <Hamburger3 />
+        <h1 className="history-title">REGISTRO</h1>
+        <div className="container-content">
 
-        <div className="content-history">
+          <div className="content-history">
+            <div className="card-history-main">
+              <img src="src\assets\cloud-home.png" alt="" className='cloud-card-main'/>
+            </div>
+            <div className="card-history1"></div>
+            <div className="card-history2"></div>
+            <div className="card-history3"></div>
+            <div className="card-history4"></div>
+            <div className="card-history5"></div>
+            <div className="card-history6"></div>
+          </div>
         </div>
       </div>
     </div>
